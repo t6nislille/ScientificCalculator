887354d8892f61ab870b92d30452af2d157c37a3
Tõnis <tonislille@gmail.com>
2023-03-26 07:24:48 +0300

    Fix WindowSize

--- ScientificCalculator/App.xaml.cs
+++ ScientificCalculator/App.xaml.cs
@@ -1,11 +1,33 @@
-﻿namespace ScientificCalculator;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace ScientificCalculator;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
 	public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+#if WINDOWS
+	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (Handler, View) =>
+	{
+		var mauiWindow = handler.VirtualView;
+		var nativeWindow = handler.PlatformView;
+		nativeWindow.Activate();
+		IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight)); 
+	});
+#endif
+
+
+        MainPage = new AppShell();
 	}
 }
